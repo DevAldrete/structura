9fb52b9b131420f8eab80e007967ca159ad560d3
DevAldrete <aldretelearns@gmail.com>
2026-08-14 20:17:38 -0600

    added netlify support for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import netlifyReactRouter from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+  plugins: [tailwindcss(), reactRouter(), netlifyReactRouter()],
+  resolve: { tsconfigPaths: true },
 });
